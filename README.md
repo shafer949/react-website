cebffc2b0721fd1a01dc456ab704405e1c4791f8
Meghan <shafer949@gmail.com>
2020-03-11 15:47:58 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-website
-Created with CodeSandbox
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles.css";
+
+const Header = () => {
+  return (
+    <div className="HeaderDiv">
+      <header className="header">
+        <div>Title</div>
+        <button>Click</button>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
